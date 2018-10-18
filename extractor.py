2f13f931ce130d52b598b4faa51d5cea8810445a
KaiKangSDU <1347746564@qq.com>
2018-10-18 10:36:55 +0800

    load premodel parameter

--- extractor.py
+++ extractor.py
@@ -4,19 +4,19 @@ from VGG_Face_torch import VGG_Face_torch\
 
 
 
-
+#搭建了vgg face 模型
 
 class VGG_Net(nn.Module):
     def __init__(self, model):
         super(VGG_Net, self).__init__()
-
-        self.pre_model = nn.Sequential(*list(model.children())[:-1])
+        self.pre_model = nn.Sequential(*list(model.children())[:-1])  #删除最后一层
         # self.dropout = nn.Dropout(p=0.8)
         self.classifier = nn.Linear(4096, 7)
 
     def forward(self, x):
         x = self.pre_model(x)
         # x = self.dropout(x)
+
         x = self.classifier(x)
 
         return x
@@ -30,9 +30,7 @@ else:
 
 
 
-
-
-
+model.load_state_dict(torch.load("best_vggface.pkl",map_location='cpu'))
 
 
 
